7866d8fcb9f0fcf10f1db0fda107cbf2a7a3664f
AndrasKovacs84 <ka841001@gmail.com>
2017-11-22 10:02:56 +0100

    solution

--- LightningWordFinder.hpp
+++ LightningWordFinder.hpp
@@ -9,7 +9,22 @@ class LightningWordFinder
 public:
     std::string getLongestWord(const std::string& text)
     {
-        return "Longest Word";
+        std::string longestWord = "";
+        std::string nextWordToCheck = "";
+        char currentLetter;
+        for(std::string::size_type i = 0; i < text.size(); ++i) {
+            currentLetter = text[i];
+            if(currentLetter <= 'z' && currentLetter >= 'A') {
+                nextWordToCheck += currentLetter;
+
+                if(nextWordToCheck.size() > longestWord.size()) {
+                    longestWord = nextWordToCheck;
+                }
+            } else {
+                nextWordToCheck = "";
+            }
+        }
+        return longestWord;
     }
 };
 #endif //CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
